b4a3e7ebc970be0dc7879df700c047c2909ee871
SAGARIKA28 <saga28r@gmail.com>
2020-05-04 08:23:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PopUp from "./components/popUp";
 import M from "materialize-css";
 import CarList from "./components/carList";
 import CarDetail from "./components/carDetail";
+import Errors from "./components/errors";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export default class App extends Component {
   render() {
@@ -17,6 +18,7 @@ export default class App extends Component {
             <Route path="/" exact component={CarList} />
             <Route path="/car/:id" component={CarDetail} />
             <Route path="/book/:id" component={BookinForm} />
+            <Route component={Errors} />
           </Switch>
           <Footer />
         </Router>
