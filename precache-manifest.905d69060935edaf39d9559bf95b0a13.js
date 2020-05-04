acbf6c50f06ebdc456b950f0f10d2fadff0f4b5e
SAGARIKA28 <saga28r@gmail.com>
2020-05-04 09:23:07 +0530

    Updates

---
+++ precache-manifest.905d69060935edaf39d9559bf95b0a13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e1b5d691484979329b4d72ebda5966",
+    "url": "/RentalCarProject/index.html"
+  },
+  {
+    "revision": "6daabe22824060c33046",
+    "url": "/RentalCarProject/static/css/2.59405167.chunk.css"
+  },
+  {
+    "revision": "22d16eeb3d7d86b2028d",
+    "url": "/RentalCarProject/static/css/main.23ddf263.chunk.css"
+  },
+  {
+    "revision": "6daabe22824060c33046",
+    "url": "/RentalCarProject/static/js/2.b78de845.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/RentalCarProject/static/js/2.b78de845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d16eeb3d7d86b2028d",
+    "url": "/RentalCarProject/static/js/main.e52aa9e2.chunk.js"
+  },
+  {
+    "revision": "714f5dcd0234bee7338e",
+    "url": "/RentalCarProject/static/js/runtime-main.8a9e43ec.js"
+  }
+]);
